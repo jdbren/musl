62d732499cc50533936aa79ec4a2aed2883fb2d3
Jackson Brenneman <jdb360@georgetown.edu>
2026-02-22 11:16:59 -0500

    add alarm

--- src/unistd/alarm.c
+++ src/unistd/alarm.c
@@ -4,7 +4,12 @@
 
 unsigned alarm(unsigned seconds)
 {
+#ifdef SYS_alarm
+	long ret = syscall(SYS_alarm, seconds);
+	return __syscall_ret(ret);
+#else
 	struct itimerval it = { .it_value.tv_sec = seconds }, old = { 0 };
 	setitimer(ITIMER_REAL, &it, &old);
 	return old.it_value.tv_sec + !!old.it_value.tv_usec;
+#endif
 }
