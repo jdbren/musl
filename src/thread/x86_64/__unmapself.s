3f7685f2690a8121cd6ec761237beb5e2ad8d016
Jackson Brenneman <jdb360@georgetown.edu>
2026-06-28 16:08:02 -0400

    fix: correct SYS_munmap and SYS_exit syscall numbers in __unmapself

--- src/thread/x86_64/__unmapself.s
+++ src/thread/x86_64/__unmapself.s
@@ -3,8 +3,8 @@
 .global __unmapself
 .type   __unmapself,@function
 __unmapself:
-	movl $11,%eax   /* SYS_munmap */
+	movl $45,%eax   /* SYS_munmap */
 	syscall         /* munmap(arg2,arg3) */
 	xor %rdi,%rdi   /* exit() args: always return success */
-	movl $60,%eax   /* SYS_exit */
+	movl $1,%eax   /* SYS_exit */
 	syscall         /* exit(0) */
